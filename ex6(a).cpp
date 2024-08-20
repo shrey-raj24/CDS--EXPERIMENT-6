832ce2a4c15897bca681fe4e0ecb82f1ba42dc9a
Shrey raj <shreyraj37@gmail.com>
2024-08-20 22:09:18 +0530

    Add files via upload

---
+++ ex6(a).cpp
@@ -0,0 +1,13 @@
+//SHREY RAJ
+//23070123123
+//EXPERIMENT-6(a)
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for (i=0;i<10;i++) {
+    cout <<"HELLO WORLD("<<i+1<<")"<<"\n";
+    }
+    return 0;
+}
