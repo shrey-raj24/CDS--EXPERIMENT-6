58349195db46257dab59faf365cd0077935cdbf1
Shrey raj <shreyraj37@gmail.com>
2024-08-20 23:49:54 +0530

    Add files via upload

---
+++ ex6(c).cpp
@@ -0,0 +1,17 @@
+// NAME - SHREY RAJ
+// PRN - 23070123123
+// EXPERIMENT -6(C) 
+
+#include<iostream> 
+using namespace std;
+
+int main() 
+{
+    int a=0;
+    do {
+        cout<< a << "\n";
+        a=a+2;
+    }
+    while (a<=50); 
+    return 0; 
+}
