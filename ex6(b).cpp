4f256b28826480382278000502bf11be9b8f1ec0
Shrey raj <shreyraj37@gmail.com>
2024-08-20 23:53:54 +0530

    Add files via upload

---
+++ ex6(b).cpp
@@ -0,0 +1,19 @@
+// NAME - SHREY RAJ
+// PRN - 23070123123 
+// EXPERIMENT - 6(B) 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"Enter a number: "<<"\n";
+    cin>>i;
+    while(i<=10)
+    {
+        cout<<" "<<"\n"<<i++;
+    
+    }
+    return 0;
+}
